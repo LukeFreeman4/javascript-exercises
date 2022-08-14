9893828a8b6c02ab9ac67c71a96df7d748e39c73
Luke <luke.freeman4@gmail.com>
2022-08-14 18:28:00 -0500

    update

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(msg, num) {
+    if (num < 0) {
+        return "ERROR"
+    } else {
+        return msg.repeat(num)
+    }
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(myArray, ...num) {
+    removeIndex = num - 1
+    for (i in num) {
+        myArray.splice(removeIndex, 1)
+        return myArray
+    }
+    
 };
 
 // Do not edit below this line
