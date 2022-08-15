e4123b3ffae180a388e45d6019d6eb6622260309
Luke <luke.freeman4@gmail.com>
2022-08-14 19:02:24 -0500

    removed code from test2 for testing

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,9 +1,5 @@
 const removeFromArray = function(myArray, ...num) {
-    removeIndex = num - 1
-    for (i in num) {
-        myArray.splice(removeIndex, 1)
-        return myArray
-    }
+    
     
 }
 ;
