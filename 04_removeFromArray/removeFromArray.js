262a0e41e42c279b4271ea675565a228d77e5896
Luke <luke.freeman4@gmail.com>
2022-08-14 19:12:01 -0500

    changes

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,8 +1,12 @@
-const removeFromArray = function(myArray, ...num) {
-    
+const removeFromArray = function() {
+
     
 }
 ;
+
+for (var i = removeFromArray; i>=0; i--) {
+    myArray.splice(removeFromArray[i],1)
+};
 
 // Do not edit below this line
 module.exports = removeFromArray;
