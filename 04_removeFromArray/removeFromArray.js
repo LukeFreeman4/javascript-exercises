cfc2b25d23a35df2d17d0196b1d9ade2279c945b
Luke <luke.freeman4@gmail.com>
2022-08-17 18:05:20 -0500

    update

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,11 +1,11 @@
-const removeFromArray = function(myArray, ...num) {
-    for (var i = removeFromArray; i>=0; i--) {
-        myArray.splice(removeFromArray[i],1)
+const removeFromArray = function(arg1, arg2) {
+    var index = arg1.indexof(arg2)
+    if (inex > -1) {
+        arg1.splice(index, 1)
     }
     
-}
-;
-// Look into array.filter()
+};
+// Look into array.filtr()
 // though most likely will need a for loop. consider previous code that worked for removing 1 element. 
 
 // Do not edit below this line
